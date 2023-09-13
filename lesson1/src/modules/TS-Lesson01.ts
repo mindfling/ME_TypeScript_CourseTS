41bd4ddd99d67a5ff9007af77e69b9959632ad2d
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-09-13 23:10:28 +0300

    Create TS-Lesson01.ts

---
+++ lesson1/src/modules/TS-Lesson01.ts
@@ -0,0 +1,30 @@
+const car = {
+  brand: 'BMW',
+  model: 'xDrive30e',
+  country: 'Germany',
+  price: 50_000,
+  color: 'white',
+  rating: [
+    ['performance', '8.0'],
+    ['comfort', '8.0'],
+    ['interior', '8.5'],
+    ['technology', '8.0'],
+    ['storage', '8.0'],
+    ['fuelEconomy', '6.5'],
+    ['value', '7.5'],
+    ['wildcard', '8.5'],
+  ],
+
+};
+
+const totalVerdict = (car) => {
+  const sumRating = car.rating.reduce((acc, item) => {
+    console.log(acc, item);
+
+    return item[1] + acc;
+  }, 0);
+  const verdict = sumRating / car.rating.length;
+  return verdict;
+};
+
+export const carResult = totalVerdict(car);
