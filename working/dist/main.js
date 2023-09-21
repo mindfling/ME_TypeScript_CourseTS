3864df38557d05c6368d295debf4e7ea9b0534c2
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-09-21 21:23:52 +0300

    work with class constructor

--- working/dist/main.js
+++ working/dist/main.js
@@ -1,5 +1,11 @@
 "use strict";
 console.log('hello lesson4');
 class Student {
+    name;
+    course;
+    constructor(name, course) {
+        this.name = name;
+        this.course = course;
+    }
 }
-const student1 = new Student();
+const student1 = new Student('Дмитри', 'Верстка сайтов');
--- working/src/main.ts
+++ working/src/main.ts
@@ -1,8 +1,15 @@
 console.log('hello lesson4');
 
 class Student {
+  name: string;
+  course: string;
+  
+  constructor(name: string, course: string) {
+    this.name = name;
+    this.course = course;
+  }
 }
 
 
 // экземпляр класса
-const student1: Student = new Student();
+const student1: Student = new Student('Дмитри', 'Верстка сайтов');
