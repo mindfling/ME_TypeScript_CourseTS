eb3d8005b096c78da2176f7d1023f74c25d6b415
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-09-21 21:23:24 +0300

    working simple class

---
+++ working/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hello lesson4');
+class Student {
+}
+const student1 = new Student();
---
+++ working/src/main.ts
@@ -0,0 +1,8 @@
+console.log('hello lesson4');
+
+class Student {
+}
+
+
+// экземпляр класса
+const student1: Student = new Student();
