df54e7c69bc425ab5881050d0a53b8790982fad4
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-09-29 20:39:38 +0300

    Update SellingSimple.ts

--- lesson4/task42/src/SellingSimple.ts
+++ lesson4/task42/src/SellingSimple.ts
@@ -1,23 +1,30 @@
+// * простой класс для простой продажи
 import { AbstractSelling } from "./AbstractSelling";
+import { Product } from "./Product";
 
-
-// 1. Реализуйте первый производный класс от AbstractSelling, в котором продажа товара осуществляется с точной скидкой от цены товара размер скидки 10$. Переопределите нужные методы.
 
 export class SellingSimple extends AbstractSelling {
-  private money: number = 0;
+  private money: number = 0; // поумолчанию 0
 
   public getPrice: () => string = () => {
     this.money = this.amount * this.product.price;
-    // this.product
-    // this.amount
-  
-    if (this.money > 10) {
-      return (this.money - 10).toFixed(2);
-    } else {
-      // money <= 10
+      // возвращает строку от числа с 2 знаками после запятой
       return (this.money).toFixed(2);
-    }
-    // возвращает строку от числа с 2 знаками после запятой
   }
 
 }
+
+/*
+* *test
+const product = new Product('Тестовый продукт', 1230);
+const sell = new SellingSimple(product, 10);
+
+product.price;
+product.title;
+
+sell.product.title;
+sell.product.price;
+
+sell.amount;
+sell.getPrice();
+*/
