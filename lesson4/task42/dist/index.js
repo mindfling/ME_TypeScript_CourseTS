5b353b62296096f2d2b0efb5d9c8e420c28f78aa
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-05 20:56:50 +0300

    feat index try to sort

--- lesson4/task42/dist/index.js
+++ lesson4/task42/dist/index.js
@@ -2,17 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Product_1 = require("./Product");
 const SellingSimple_1 = require("./SellingSimple");
+const SellingTenDiscount_1 = require("./SellingTenDiscount");
+const SellingPercentDiscount_1 = require("./SellingPercentDiscount");
 console.log();
 const apple = new Product_1.Product('Яблоки сезонные', 69.99);
 const banana = new Product_1.Product('Бананы загранишные', 99.99);
 const tomato = new Product_1.Product('Томаты помидоры', 59.00);
+const onion = new Product_1.Product('Лук репчаты', 32.00);
 console.log({ apple });
 console.log({ banana });
 console.log({ tomato });
-console.log("товар:");
-console.log("название: ", apple.title);
-console.log("цена: ", apple.price, " ру за кг");
+console.log({ onion });
+const sellingApple = new SellingTenDiscount_1.SellingTenDiscount(apple, 10);
+const sellingBanana = new SellingSimple_1.SellingSimple(banana, 25);
+const sellingOnion = new SellingPercentDiscount_1.SellingPercentDiscount(onion, 15, 10);
+const listOfSellings = [
+    new SellingTenDiscount_1.SellingTenDiscount(apple, 10),
+    new SellingTenDiscount_1.SellingTenDiscount(banana, 5),
+    new SellingTenDiscount_1.SellingTenDiscount(tomato, 15),
+    new SellingTenDiscount_1.SellingTenDiscount(onion, 7),
+    new SellingPercentDiscount_1.SellingPercentDiscount(apple, 2, 10),
+    new SellingPercentDiscount_1.SellingPercentDiscount(banana, 5, 10),
+    new SellingPercentDiscount_1.SellingPercentDiscount(tomato, 15, 10),
+    new SellingPercentDiscount_1.SellingPercentDiscount(onion, 7, 10),
+];
+console.log('unsorted ListOfSellings: ', listOfSellings);
 console.log();
-const sellingBanana = new SellingSimple_1.SellingSimple(banana, 24);
-console.log('sellingBanana: ', sellingBanana);
-console.log('цена продукта', sellingBanana.product.title, '\nсумма продажи', sellingBanana.amount, 'кг = ', sellingBanana.getPrice(), 'ру');
--- lesson4/task42/src/index.ts
+++ lesson4/task42/src/index.ts
@@ -3,30 +3,43 @@
 import { Product } from "./Product";
 import { AbstractSelling } from "./AbstractSelling";
 import { SellingSimple } from "./SellingSimple";
+import { SellingTenDiscount } from "./SellingTenDiscount";
+import { SellingPercentDiscount } from "./SellingPercentDiscount";
 
 console.log();
 
 // создаем продукт
 const apple: Product = new Product('Яблоки сезонные', 69.99);
 const banana: Product = new Product('Бананы загранишные', 99.99);
+const tomato: Product = new Product('Томаты помидоры', 59.00);
+const onion: Product = new Product('Лук репчаты', 32.00);
 
 
 console.log({ apple });
-
-console.log("товар:");
-console.log("название: ", apple.title);
-console.log("цена: ", apple.price, " ру за кг");
-// apple.logInfo();
+console.log({ banana });
+console.log({ tomato });
+console.log({ onion });
 
 
+// * продаём
+const sellingApple: AbstractSelling = new SellingTenDiscount(apple, 10);
+const sellingBanana: AbstractSelling = new SellingSimple(banana, 25);
+const sellingOnion: AbstractSelling = new SellingPercentDiscount(onion, 15, 10);
+
+const listOfSellings: AbstractSelling[] = [
+  new SellingTenDiscount(apple, 10),
+  new SellingTenDiscount(banana, 5),
+  new SellingTenDiscount(tomato, 15),
+  new SellingTenDiscount(onion, 7),
+  new SellingPercentDiscount(apple, 2, 10),
+  new SellingPercentDiscount(banana, 5, 10),
+  new SellingPercentDiscount(tomato, 15, 10),
+  new SellingPercentDiscount(onion, 7, 10),
+]
+
+console.log('unsorted ListOfSellings: ', listOfSellings);
 console.log();
 
-// console.log({banana});
-// * продаём
-const sellingBanana: AbstractSelling = new SellingSimple(banana, 24);
-console.log('sellingBanana: ', sellingBanana);
-console.log('цена продукта', sellingBanana.product.title,
-  '\nсумма продажи', sellingBanana.amount, 'кг = ', sellingBanana.getPrice(), 'ру');
-
-
-
+// todo Question КАК сделать стортировку массива ?
+// console.log('sorted ListOfSellings:', Array.sort(AbstractSelling.compare));
+  
