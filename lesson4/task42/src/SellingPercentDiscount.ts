505e9fb1b7e4c14037ae4bd8fae317e100b385bb
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-05 19:44:18 +0300

    Update SellingPercentDiscount.ts

--- lesson4/task42/src/SellingPercentDiscount.ts
+++ lesson4/task42/src/SellingPercentDiscount.ts
@@ -1,31 +1,34 @@
+// * класс с процентной скидкой -10%
 import { AbstractSelling } from "./AbstractSelling";
 import { Product } from "./Product";
 
 
-export class SellingForAmount extends AbstractSelling {
+export class SellingPercentDiscount extends AbstractSelling {
   private minimalAmount: number;
 
 
-  constructor(sellingProduct: Product, sellingAmount: number, minimalAmount: number) {
-    super(sellingProduct, sellingAmount);
-    this.product = sellingProduct; // ?
-    this.amount = sellingAmount;  // ?
+  constructor(product: Product, amount: number, minimalAmount: number) {
+    super(product, amount);
+    this.product = product
+    this.amount = amount;
     this.minimalAmount = minimalAmount;
   }
 
   public getPrice: () => string = () => {
-    if (this.amount < this.minimalAmount) { // условие получения скидки
-      return (this.amount * this.product.price).toFixed(2);
-    } else {
+    if (this.amount >= this.minimalAmount) { // условие получения скидки
       return (this.amount * this.product.price * 0.9).toFixed(2); // -10% discount
+    } else {
+      return (this.amount * this.product.price).toFixed(2);
     }
   }
-  
 }
 
 
+/*
+* test
 const prod = new Product('Мой продукт', 124);
 console.log('prod: цена', prod.price, " название", prod.title);
 
-const sell = new SellingForAmount(prod, 10, 5);
+const sell = new SellingPercentDiscount(prod, 10, 5);
 console.log('sell: ', sell);
+*/
